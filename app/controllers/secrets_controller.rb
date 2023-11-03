dc0a80cebbc47d1f71460e17f425275a10310de4
parth-patki <140078943+parth-patki@users.noreply.github.com>
2023-11-03 16:29:51 +0530

    Added  Index to Secret controller

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,10 @@
 class SecretsController < ApplicationController
   before_action :authenticate_request
 
+  def index
+    @users = User.all
+  end
+
   def secret_action
     render json: { message: "Welcome, #{current_user.username}!" }
   end
